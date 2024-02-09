1c9f0b81b12fefab3089a5e4ef8b213f8b942855
TinNguyen <ductin16111@gmail.com>
2024-02-09 23:28:20 +0200

    add pagination feature

---
+++ src/features/pagination/Page.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { fetchBreweries } from "../breweries/brewerySlice";
+import Spinner from "../../components/Spinner";
+import { Link } from "react-router-dom";
+
+type Props = {
+    page: number
+}
+
+const Page: React.FC<Props> = ({ page: number }) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+	const dispatch = useAppDispatch();
+	const breweryList = useAppSelector((state) => state.brewery.breweryList);
+
+	useEffect(() => {
+		setIsLoading(true);
+		dispatch(fetchBreweries(1, 4));
+		setIsLoading(false);
+	}, []);
+
+    return (
+        <>
+            {isLoading ? (
+				<Spinner />
+			) : (
+				<>
+					{breweryList.map((brewery) => (
+						<Link to={`/${brewery.id}`} key={brewery.id}>
+							<li>
+								{brewery.name}
+								<img src={brewery.img} alt="" />
+							</li>
+						</Link>
+					))}
+				</>
+			)}
+        </>
+    )
+}
+
+export default Page
--- src/features/routing/Home.tsx
+++ src/features/routing/Home.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { Link } from "react-router-dom";
-import { fetchBreweries } from "../breweries/brewerySlice";
-import Spinner from "../../components/Spinner";
+import MyPagination from "../pagination/MyPagination";
+import Page from "../pagination/Page";
 
 const Home: React.FC = () => {
-	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const dispatch = useAppDispatch();
-	const breweryList = useAppSelector((state) => state.brewery.breweryList);
-
-	useEffect(() => {
-		setIsLoading(true);
-		dispatch(fetchBreweries(1, 4));
-		setIsLoading(false);
-	}, []);
 	return (
 		<>
-			{isLoading ? (
-				<Spinner />
-			) : (
-				<>
-					{breweryList.map((brewery) => (
-						<Link to={`/${brewery.id}`} key={brewery.id}>
-							<li>
-								{brewery.name}
-								<img src={brewery.img} alt="" />
-							</li>
-						</Link>
-					))}
-				</>
-			)}
+            <Page page={2}/>
+            <MyPagination />
 		</>
 	);
 };
